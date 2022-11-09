08872ba0170f0aaa21602d8c96c3d0e3a84cf722
zjs6631 <zacharyjonessmith@gmail.com>
2022-11-09 12:13:06 -0500

    moved state of shop into app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -5,8 +6,55 @@ import { Home } from './components/Home';
 import Shop from './components/Shop';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import axeImg from "./images/axe.jpeg";
+import bearsprayImg from "./images/bearspray.jpg";
+import bugsprayImg from "./images/bugspray.jpg";
+import cordImg from "./images/cord.jpg";
+import firestarterImg from "./images/firestarter.jpg";
+import lanternImg from "./images/lantern.jpeg";
+import tentImg from "./images/tent.jpg";
+import uniqid from "uniqid";
 
 function App() {
+
+  const [cart, setCart] = useState([]); //create cart to hold items to buy
+
+  //create factory function to create objects for each item in shop
+  const itemFactory = (key, name, img, price, numInCart) =>{
+      return {key, name, img, price, numInCart};
+  }
+
+  //create objects to represent each item and their data
+  const axe = itemFactory(uniqid(), "Axe", axeImg, 20.99, 0);
+  const bearspray = itemFactory(uniqid(), "Bear Spray", bearsprayImg, 15.85, 0);
+  const bugspray = itemFactory(uniqid(), "Bug Spray", bugsprayImg, 5.99, 0);
+  const cord = itemFactory(uniqid(), "Utility Cord", cordImg, 5.00, 0);
+  const firestarter = itemFactory(uniqid(), "Fire Starter", firestarterImg, 10.50, 0);
+  const lantern = itemFactory(uniqid(), "Oil Lantern", lanternImg, 13.99, 0);
+  const tent = itemFactory(uniqid(), "Tent", tentImg, 55.00, 0);
+
+  //inventory state to render the items information on screen
+  const [inventory, setInventory] = useState([axe, bearspray, bugspray, cord,
+  firestarter, lantern, tent]);
+
+
+  const handleClick = (e) =>{
+      let copyCart = cart;
+      inventory.map((item)=>{
+          if(item.key == e.currentTarget.id){
+              item.numInCart+=1;
+              copyCart.push(item);
+              
+          }
+      });
+      setCart(copyCart);
+      console.log(cart);
+  }
+
+  useEffect(()=>{
+    
+  },[cart])
+
   return (
     <div className='App'>
 
@@ -14,7 +62,7 @@ function App() {
       
       <Routes>
         <Route index element = {<Home />}/>
-        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/shop" element={<Shop handleClick={handleClick} cart={cart} inventory={inventory}/>}/>
       </Routes>
       
       <Footer />
