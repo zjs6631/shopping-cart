c8d2fb02233990bade263212ddfd13f42f9fa912
zjs6631 <zacharyjonessmith@gmail.com>
2022-11-09 21:26:12 -0500

    finished styling and added github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import github from "../images/icons8-github-48.png";
 
 const Footer = () =>{
 
     return(
         <div className="Footer">
             <p>Created by: Zachary Smith</p>
+            <a href="https://github.com/zjs6631/shopping-cart" target="_blank"><div className='github'><img src={github}/></div></a>
         </div>
     )
 }
